baa13555ed348bb4a257f475f526acebde012f5d
Dmitriy Borovoy <d.borovoi@andersenlab.com>
2016-12-06 15:50:31 +0300

    yes bitch i #resolve you again

--- src/main/java/SuperClass.java
+++ src/main/java/SuperClass.java
@@ -1,6 +1,6 @@
 /**
  * Created by User on 02.12.2016.
- * comment1123456567454545ssssssrrrrrqqqqqffffffffff
+ * comment1123456567454545ssssssrrrrrqqqqqffffffffffffffffss
  */
 public class SuperClass {
 
